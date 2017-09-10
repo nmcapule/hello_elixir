d2a60b72c651ebe958d3021fc26b041d5fe6c04f
Nathaniel M. Capule <llss.s.sls.lll@gmail.com>
2017-09-10 19:19:02 +0800

    setup on test

--- mixymix/test/bucket_test.exs
+++ mixymix/test/bucket_test.exs
@@ -1,8 +1,12 @@
 defmodule KV.BucketTest do 
   use ExUnit.Case, async: true
 
-  test "stores values by key" do
-    {:ok, bucket} = start_supervised KV.Bucket
+  setup do
+    {:ok, bucket} = start_supervised(KV.Bucket)
+    %{bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 3)
